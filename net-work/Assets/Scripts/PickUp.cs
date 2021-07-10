b2608a26986a7a0686f7044b88ef5d80a460fb0c
Ebdabrarlaluh <aa.ebra.61@gmail.com>
2021-07-10 20:14:06 +0300

    Kablo
    Kablo assetleri ve kabloları toplama mekaniği eklendi

---
+++ net-work/Assets/Scripts/PickUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pickUpText;
+    private bool pickUpAllowed;
+
+    private void Start()
+    {
+        pickUpText.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if(pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+        {
+            PickObject();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(true);
+            pickUpAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            pickUpText.gameObject.SetActive(false);
+            pickUpAllowed = false;
+        }
+    }
+
+    void PickObject()
+    {
+        Destroy(gameObject);
+    }
+   
+}
